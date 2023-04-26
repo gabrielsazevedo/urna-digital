f73342f18104554af4775f64c57cb5dbc51ec917
gabrielsazevedo <gabriel.azeve04@gmail.com>
2023-04-26 11:24:30 -0300

    visual updated

--- script.js
+++ script.js
@@ -27,3 +27,28 @@ function corrigir() {
 
 }
 
+function confirmar() {
+    
+    switch (num) {
+        case "12":
+            alert("Você votou em Ivete Sangalo!");
+            break;
+        case "21":
+            alert("Você votou em Leo Santana!");
+            break;
+        case "32":
+            alert("Você votou em Wilson Café!");
+            break;
+        case "23":
+            alert("Você votou em Glaucya Boechat!");
+            break;
+        case "40":
+            alert("Você votou em Negão da Viola!");
+            break;
+        default:
+            alert("Candidato não existente, tente novamente!");
+    }
+
+    corrigir();
+
+}
