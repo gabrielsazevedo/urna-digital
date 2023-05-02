10689c48fff2f4beff199f2d08035ec74d3fae61
gabrielsazevedo <gabriel.azeve04@gmail.com>
2023-05-02 11:23:11 -0300

    ivete

--- script.js
+++ script.js
@@ -30,13 +30,13 @@ function corrigir() {
 function confirmar() {
 
     const fotocandidato = document.getElementById("fotocandidato");
-    const stringscandidato = document.getElementById("stringscandidato");
+    document.getElementsByClassName("gridcontainer-info")[0].style.visibility = "visible";
 
 
     switch (num) {
         case "12":
             fotocandidato.src = "https://yt3.googleusercontent.com/0vptru8uklux9U_aDNrF1UgbsZ-Fq5kxauWGBLzBmNWZR4MNDThawCNRErYFg2V1A888N9ZnohU=s900-c-k-c0x00ffffff-no-rj"
-            stringscandidato.get = "12 - Ivete Sangalo, MPB";
+            document.getElementById("stringscandidato").innerHTML = "12 - Ivete Sangalo, MPB";
             alert("Você votou em Ivete Sangalo!");
             break;
         case "21":
